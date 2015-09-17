60344cbc8411b15954fd1adf2c5dc346b590a050
Nathan Smith <nathan@hebemedia.com>
2015-09-17 17:14:27 +0100

    Hooked weather-forecast in to data api

--- app/components/stories/complete/weather-forecast/component.js
+++ app/components/stories/complete/weather-forecast/component.js
@@ -4,52 +4,138 @@ import DatamillStory from './../../story-types/datamill-story/component';
 export default DatamillStory.extend({
     tagName: 'div',
     loaded: false,
-    didInsertElement: function() {
+    author: 'Simon Zimmerman',
+    showDay: true,
+
+    didReceiveAttrs: function () {
         this.set('title', 'Weather');
         this.set('subTitle', '5-day weather forecast for Leeds');
+
         var obj = this;
-        // datamillUrl is a property containing the url for Leeds Data Mill
-            // inherited from story-types/datamill-story
-        var url = this.get('datamillUrl') + '/api/action/datastore_search?resource_id=c2bb0c3e-52fd-4183-8727-6b9f40b829f0';
-        // getData is a function inherited from 
-            // story-types/default-story (which story-types/datamill-story extends)
-            // it provides an easy way to make AJAX requests
-            // returning a Promise (.then())
-                // to which you can pass
-                // success, failure, and complete callbacks
+        var url = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/352241?res=daily&key=f74b2d5f-4fd7-450a-b410-ed56d842a209';
+
         this.getData(url)
             .then(
-                function(data){
-                    // success
-                    console.log('weather-forecast > getData > success');
-                    // data is the response Object/Array from the AJAX request
-                    var items = [];
-                    data.result.records.forEach((tmpItem) => {
+                function (data) {
+                    var days = [];
+
+                    data.SiteRep.DV.Location.Period.forEach((tmpItem) => {
                         var id = hebeutils.guid();
-                        var item = {
-                            id: id,
-                            street: tmpItem.Street,
-                            ward: tmpItem.Ward,
-                            lat: (tmpItem.Lat == null ? tmpItem["Lat "] : tmpItem.Lat),
-                            lng: tmpItem.Long,
-                            type: tmpItem.Type,
-                            colour: tmpItem.Colour
+                        var day = {
+                            day: {
+                                windDirection: tmpItem.Rep[0].D,
+                                //windDirectionSVG: obj.getCompassSVG(tmpItem.Rep[0].D),
+                                windGust: tmpItem.Rep[0].Gn,
+                                windSpeed: tmpItem.Rep[0].S,
+                                maxTemp: tmpItem.Rep[0].Dm,
+                                minTemp: tmpItem.Rep[1].Nm,
+                                feelsLike: tmpItem.Rep[0].Fdm,
+                                humidity: tmpItem.Rep[0].Hn,
+                                visibility: tmpItem.Rep[0].V,
+                                rainProb: tmpItem.Rep[0].PPd,
+                                uv: tmpItem.Rep[0].U,
+                                weather: obj.getWeatherType(tmpItem.Rep[0].W),
+                                weatherIcon: obj.getWeatherType(tmpItem.Rep[0].W, true)
+                                //weatherDescription: obj.getWeatherTypes(tmpItem.Rep[0].W)
+                            },
+                            night: {
+                                windDirection: tmpItem.Rep[1].D,
+                                //windDirectionSVG: obj.getCompassSVG(tmpItem.Rep[1].D),
+                                windGust: tmpItem.Rep[1].Gm,
+                                wiSpeed: tmpItem.Rep[1].S,
+                                maxTemp: tmpItem.Rep[0].Dm,
+                                minTemp: tmpItem.Rep[1].Nm,
+                                feelsLike: tmpItem.Rep[1].Fnm,
+                                humidity: tmpItem.Rep[1].Hm,
+                                visibility: tmpItem.Rep[1].V,
+                                rainProb: tmpItem.Rep[1].PPn,
+                                uv: tmpItem.Rep[0].U,
+                                weather: obj.getWeatherType(tmpItem.Rep[1].W),
+                                weatherIcon: obj.getWeatherType(tmpItem.Rep[1].W, true)
+                            },
+                            date: tmpItem.value.toString().ensureNoEndingString('Z')
                         };
-        
-                        items.push(item);
+                        days.push(day);
                     });
-                    obj.set('items', items);
+                    obj.set('days', days);
+                    obj.set('today', days[0]);
                     setTimeout(() => {
                         obj.set('loaded', true);
                     });
                 },
-                function(error) {
+                function (error) {
                     // failure
-                    console.log('weather-forecast > getData > error: ' + error);
+                    // console.log('five-day-forecast > getData > error: ' + error);
                 },
-                function(){
+                function () {
                     // complete
                 }
-            )
+                )
+    },
+
+    // compassSVG: function(windDirection) {
+    //   //   var points = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
+    //   //   var svg = '';
+    //   // for (i = 0; i < points.length; i++) {
+    //   //   if (windDirection === points[i]) {
+    //   //     console.log('success, direction = ' + points[i]);
+    //       // svg = 'svg-compass-' + wind[i];
+    //       return 'svg-compass-' + windDirection;
+    //       // var weatherSVG = 'svg-weather-' + i;
+    //     // }
+    //   // };
+    // }
+
+    getWeatherType: function (weatherIndex, icon) {
+        // weatherIndex = weatherIndex - 1;
+        var types = [
+            ['default', 'Clear night'],
+            ['sunny', 'Sunny day'],
+            ['cloudy', 'Partly cloudy (night)'],
+            ['cloudy', 'Partly cloudy (day)'],
+            ['default', 'Not used'],
+            ['mist', 'Mist'],
+            ['fog', 'Fog'],
+            ['cloudy', 'Cloudy'],
+            ['overcast', 'Overcast'],
+            ['light-rain', 'Light rain shower (night)'],
+            ['light-rain', 'Light rain shower (day)'],
+            ['light-rain', 'Drizzle'],
+            ['light-rain', 'Light rain'],
+            ['heavy-rain', 'Heavy rain shower (night)'],
+            ['heavy-rain', 'Heavy rain shower (day)'],
+            ['heavy-rain', 'Heavy rain'],
+            ['sleet', 'Sleet shower (night)'],
+            ['sleet', 'Sleet shower (day)'],
+            ['sleet', 'Sleet'],
+            ['hail', 'Hail shower (night)'],
+            ['hail', 'Hail shower (day)'],
+            ['hail', 'Hail'],
+            ['light-snow', 'Light snow shower (night)'],
+            ['light-snow', 'Light snow shower (day)'],
+            ['light-snow', 'Light snow'],
+            ['heavy-snow', 'Heavy snow shower (night)'],
+            ['heavy-snow', 'Heavy snow shower (day)'],
+            ['heavy-snow', 'Heavy snow'],
+            ['thunder', 'Thunder shower (night)'],
+            ['thunder', 'Thunder shower (day)'],
+            ['thunder', 'Thunder']
+        ];
+
+        var weather = types[weatherIndex];
+        if (icon == true) {
+            return weather[0] + '-grey';
+        } else {
+            return weather[1];
+        }
+
+        console.log(weather[0]);
+    },
+
+    actions: {
+        togglePeriod(period) {
+            this.set('showDay', (period == 'day'));
+        }
     }
 });
+
