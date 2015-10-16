8205776db69d60d340f36037c2283008166f50ea
Ste Allan <ste@hebeworks.com>
2015-10-16 16:34:38 +0100

    Refactored thumb list component.

--- app/components/stories/story-library/list-view-thumbnail/component.js
+++ app/components/stories/story-library/list-view-thumbnail/component.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	tagName: 'li',
-	classNames: 'thumb',
+    attributeBindings: ['cpn-thumb-list_item'],
+    'cpn-thumb-list_item': '',
 	
 	click: function() {
 		this.sendAction('action', this.model);
