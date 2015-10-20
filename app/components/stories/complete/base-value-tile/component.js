f6b43f0c5557b0d60487fe29587c28167374fbba
Ste Allan <ste@hebeworks.com>
2015-10-20 16:32:24 +0100

    Added value tile and updated RAG tile stories.

--- app/components/stories/complete/base-value-tile/component.js
+++ app/components/stories/complete/base-value-tile/component.js
@@ -4,11 +4,11 @@ import DatamillStory from './../../story-types/datamill-story/component';
 export default DatamillStory.extend({
     tagName: 'div',
     loaded: false,
-    ragRating: '-lime', // -lime (green), -yellow (amber), -red (red)
+    color: '-white',
     tileValue: '4337',
     tileDesc1: 'Parking fines',
     tileDesc2: 'Issued this month',
-    tileShade: 'light',
+    tileShade: '',
     
     didInsertElement: function() {
         
