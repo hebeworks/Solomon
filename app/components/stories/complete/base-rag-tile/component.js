d9a3c09473ec3ee483f7f6b6907dc184cfcc133a
Ste Allan <ste@hebeworks.com>
2015-10-20 14:31:22 +0100

    Removed redundant logic from Base RAG Tile story.

--- app/components/stories/complete/base-rag-tile/component.js
+++ app/components/stories/complete/base-rag-tile/component.js
@@ -4,24 +4,12 @@ import DatamillStory from './../../story-types/datamill-story/component';
 export default DatamillStory.extend({
     tagName: 'div',
     loaded: false,
-    ragRating: 'green', // red, amber or green
+    ragRating: '-lime', // -lime (green), -yellow (amber), -red
     ragValue: '4337',
     ragDesc1: 'Parking fines',
     ragDesc2: 'Issued this month',
     
     didInsertElement: function() {
-        this.setRating();
-    },
-    
-    setRating: function() {
-        var obj = this;
         
-        if (this.ragRating == 'green') {
-            obj.set('ragRating', '-lime');
-        } else if (this.ragRating == 'amber') {
-            obj.set('ragRating', '-yellow');
-        } else {
-            obj.set('ragRating', '-red');
-        }
     }
 });
