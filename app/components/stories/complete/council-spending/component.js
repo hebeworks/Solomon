d4bca5a121b0fd765290bfcbca2094ec06d68a17
Nathan Smith <nathan@hebemedia.com>
2015-10-30 17:11:43 +0000

    tidied up Council Spending

--- app/components/stories/complete/council-spending/component.js
+++ app/components/stories/complete/council-spending/component.js
@@ -1,21 +1,34 @@
 import DefaultStory from './../../story-types/default-story/component';
 
 export default DefaultStory.extend({
-    // New variables 
-    _directoratePercentageRemaining: 100,
-    directoratePercentageRemaining: Ember.computed({
+    didInsertElement: function () {
+        var _this = this;
+
+        this.set('title', 'Council Spending');
+        this.set('subTitle', 'Monthly spend data by directorate');
+
+        var dateQuery = hebeutils.Base64.encode(JSON.stringify({ comparison: '$lte', value:new Date("2015-03-01") }));
+        this.loadData('date='+dateQuery).then(function (data) {
+            _this.setMonths(data);
+            _this.set('selectedMonth', data[0]);
+        });
+    },
+    
+    _catPercentageRemaining: 100,
+    catPercentageRemaining: Ember.computed({
         get(){
-            return this.get('_directoratePercentageRemaining');
+            return this.get('_catPercentageRemaining');
         },
         set(key,val) {
             var newVal = 100 - val; // because the css wants the inverse of the percentage
-            console.log('totalPercentageRemaining: '+newVal);
-            if(this.get('_directoratePercentageRemaining') != newVal) {
-                this.set('_directoratePercentageRemaining',newVal);
+            console.log('catPercentageRemaining: '+newVal);
+            if(this.get('_catPercentageRemaining') != newVal) {
+                this.set('_catPercentageRemaining',newVal);
             }
             return newVal;
         }
     }),
+    
     _totalPercentageRemaining: 100,
     totalPercentageRemaining: Ember.computed({
         get(){
@@ -23,7 +36,7 @@ export default DefaultStory.extend({
         },
         set(key,val) {
             var newVal = 100 - val; // because the css wants the inverse of the percentage
-            console.log('directoratePercentageRemaining: '+newVal);
+            console.log('totalPercentageRemaining: '+newVal);
             if(this.get('_totalPercentageRemaining') != newVal) {
                 this.set('_totalPercentageRemaining',newVal);
             }
@@ -31,46 +44,12 @@ export default DefaultStory.extend({
         }
     }),
 
-    didInsertElement: function () {
-        var _this = this;
-
-        this.set('title', 'Council Spending');
-        this.set('subTitle', 'Monthly spend data by directorate');
-
-        // setTimeout(function () {
-        //     _this.updateDirectorateSpend();
-        // }, 1500);
-
-        // setTimeout(function () {
-        //     _this.updateTotalSpend();
-        // }, 1750);
-
-        // var query = { $and: [{ "date": { $gte: new Date("2014-04-01T01:00:00+0100") }, "date": { $lte: new Date("2015-03-01") }  }] }
-        // var query = {
-        //     date: {
-        //         $gte: new Date("2014-04-01"),
-        //         $lte: new Date("2014-07-01")
-        //         // $lte: new Date("2015-03-01")
-        //     }
-        // };
-        // query = hebeutils.Base64.encode(JSON.stringify(query));
-        // this.loadData('query=' + query + '&sort=date&sortdirection=DESC').then(function (data) {
-        var dateQuery = hebeutils.Base64.encode(JSON.stringify({ comparison: '$lte', value:new Date("2015-03-01") }));
-        this.loadData('date='+dateQuery).then(function (data) {
-            _this.setMonths(data);
-            _this.set('selectedMonth', data[0]);
-        });
-    },
-
     loadData: function (query) {
         var hebeNodeAPI = this.get('hebeNodeAPI');
         return this.getData(hebeNodeAPI + '/council-spending?' + query);
     },
 
     setMonths: function (records) {
-        // var months = _.map(records,function(record){
-        //     return { text: record.date, id: record.date};
-        // })
         var yearTotal = 0; 
         records.forEach(function(month){
             month.text = moment(new Date(month.date)).format("MMM YYYY");
@@ -84,20 +63,29 @@ export default DefaultStory.extend({
         });
     },
     
-    setCatListFromMonths: function () {
-        // get the list of directorates from the first (only because we're passing "limit=1")
-        var month = this.get('months.firstObject');
+    setCatListFromMonth: function () {
+        // get the list of directorates from the currentMonth in case some months are named differently
+        var currentSelectedCat = this.get('selectedCat');
+        var currentSelectedID = (currentSelectedCat ? currentSelectedCat.id : null);
+        var currentSelectedIndex = 0;
+        var month = this.get('selectedMonth');
         var directorates = [];
         var ignore = ['total', 'date', '_id', 'text', 'id'];
+        var i = 0;
         for (var prop in month) {
             if (ignore.indexOf(prop) == -1) {
                 directorates.push({ text: prop, id: prop });
             }
+            // if this is named the same as the current selected cat
+            // grab the index to select when we reset the cats
+            if(currentSelectedID && prop == currentSelectedID) {
+                currentSelectedIndex = i;
+            }
+            i ++;
         }
         this.set('categories', directorates);
-        this.set('selectedCat', directorates[0]);
-    }.observes('months'),
-
+        this.set('selectedCat', directorates[currentSelectedIndex]);
+    }.observes('selectedMonth'),
 
     onSelectedCatChange: function () {
         var selectedMonth = this.get('selectedMonth');
@@ -106,7 +94,7 @@ export default DefaultStory.extend({
             var monthSpend = selectedMonth["total"];
     
             var percentage = this.getPercentage(monthSpend, catSpend);
-            console.log('cat percentage: ' + percentage);
+            // console.log('cat percentage: ' + percentage);
             this.set('catTotal', catSpend);
             this.set('catPercentageRemaining', percentage);
         }
@@ -116,15 +104,10 @@ export default DefaultStory.extend({
         return ((val / totalVal) * 100).toPrecisionDigits(1)
     },
     
-    // onSelectedDateChange: function () {
-    //     var selectedDate = this.get('selectedDate');
-    // }.observes('selectedDate'),
-
-
     updateTotalSpend: function () {
         var month = this.get('selectedMonth');
         var percentage = this.getPercentage(this.get('yearTotal'),month.total);
-        console.log('month percentage: ' + percentage);
+        // console.log('month percentage: ' + percentage);
         this.set('totalPercentageRemaining', percentage);
     }.observes('selectedMonth')
 });
