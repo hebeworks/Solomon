1bb0e73756a8a4429a179374830eb264332f00fe
Nathan Smith <nathan@hebemedia.com>
2015-10-30 18:30:55 +0000

    Tidied up council spending year chart

--- app/components/stories/complete/council-spending-year-line/component.js
+++ app/components/stories/complete/council-spending-year-line/component.js
@@ -11,7 +11,7 @@ export default DefaultStory.extend({
 	chartType: 'line',
 	data: [],
 
-	loadData: function() {
+	loadData: function () {
 		var obj = this;
 		// var url = 'http://localhost:8080/councilspending?date=eyJjb21wYXJpc29uIjoiJGx0IiwidmFsdWUiOiIyMDE1LTEwLTIxIn0='; //limit=13&sort=date';
 		/*
@@ -24,14 +24,18 @@ export default DefaultStory.extend({
 				url += '&sortfield=date'
 				url += '&sortdirection=asc' OR '&sortdirection=desc' 
 		*/
-		var query = hebeutils.Base64.encode(JSON.stringify({ date: { $gt: new Date("2013-04-01") } }));		
+		var query = hebeutils.Base64.encode(JSON.stringify({ date: { $gt: new Date("2013-04-01") } }));
 		var url = 'http://localhost:8080/councilspending?query=' + query;
 		this.getData(url)
 			.then(
-				function(data){
-					obj.set('data',data);
+				function (data) {
+					obj.set('data', data);
+					var to = moment(new Date(data[0].date)).format('MMM YYYY');
+					var fromDate = moment(new Date(_.last(data).date)).format('MMM YYYY'); 
+					var subTitle = 'From ' +  fromDate + ' to ' + to;
+					obj.set('subTitle',subTitle);
 				},
-				function(err){
+				function (err) {
 					// alert(err);
 				}
 			)
@@ -65,190 +69,48 @@ export default DefaultStory.extend({
 		}
 	}),
 
-	chartOptions: {
-		low: 0,
-		showArea: false,
-		showPoint: true,
-		fullWidth: true,
-		lineSmooth: Chartist.Interpolation.simple({
-			divisor: 20
-		}),
-		axisX: {
-			labelInterpolationFnc: function (value, index) {
-				return index % 2 === 0 ? value : null;
+	chartOptions: Ember.computed(function() {
+		var _this = this;
+		var options = {
+			low: 0,
+			showArea: false,
+			showPoint: true,
+			fullWidth: true,
+			lineSmooth: Chartist.Interpolation.simple({
+				divisor: 20
+			}),
+			axisX: {
+				labelInterpolationFnc: function (value, index) {
+					return index % 2 === 0 ? value : null;
+				}
+			},
+			axisY: {
+				// Lets offset the chart a bit from the labels
+				offset: 30,
+				// The label interpolation function enables you to modify the values
+				// used for the labels on each axis. Here we are converting the
+				// values into million pound.
+				labelInterpolationFnc: function (value) {
+					var val = _this.formatLargeNumbers(value);
+					return val;
+				}
 			}
+		};
+		return options;
+	}),
+	
+	formatLargeNumbers: function (rep) {
+		rep = rep+''; // coerce to string
+		if (rep < 1000) {
+			return rep; // return the same number
 		}
-	},
-
-	// onChartChanged: function () {
-	// 	var chart = this.get('chart');
-	// 	if (this.get('chart') != null) {
-	// 		chart.on('draw', function (data) {
-	// 			if (data.type === 'line' || data.type === 'area') {
-	// 				data.element.animate({
-	// 					d: {
-	// 						begin: 2000 * data.index,
-	// 						dur: 2000,
-	// 						from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
-	// 						to: data.path.clone().stringify(),
-	// 						easing: Chartist.Svg.Easing.easeOutQuint
-	// 					}
-	// 				});
-	// 			}
-	// 		});
-
-	// 		this.$('foreignobject').each(function() {
-	// 			alert('test');
-	// 			this.addClass('ste');
-	// 		});
-	// 	}
-	// }.observes('chart'),
-
-	// data: [
-	// 	{
-	// 		"City Development": 9194109.47,
-	// 		"Childrens Services": 9375227.61,
-	// 		"Central & Corporate": 7884912.28,
-	// 		"Joint Committees And Other Bodies": 3584194.1,
-	// 		"date": {
-	// 			"$date": 1406851200000
-	// 		},
-	// 		"Social Services Pooled Budgets": 8096601.04,
-	// 		"Environment & Housing": 13746825.29,
-	// 		"Strategic Accounts": 32229503.55,
-	// 		"Adult Social Care": 9539717.65
-	// 	},
-	// 	{
-	// 		"City Development": 21751136.26,
-	// 		"Childrens Services": 23999791.77,
-	// 		"Central & Corporate": 8358398.37,
-	// 		"Joint Committees And Other Bodies": 3661200.1,
-	// 		"date": {
-	// 			"$date": 1417392000000
-	// 		},
-	// 		"Social Services Pooled Budgets": 5625975.89,
-	// 		"Environment & Housing": 14350727.57,
-	// 		"Strategic Accounts": 34789651.18,
-	// 		"Adult Social Care": 9757796.05,
-	// 		"Strategic Landlord": 4449376.52
-	// 	},
-	// 	{
-	// 		"City Development": 10152823.96,
-	// 		"Childrens Services": 9776480.92,
-	// 		"Central & Corporate": 7466827.81,
-	// 		"date": {
-	// 			"$date": 1422748800000
-	// 		},
-	// 		"Social Services Pooled Budgets": 5840297.39,
-	// 		"Environment & Housing": 7384273.12,
-	// 		"Strategic Accounts": 5198047.73,
-	// 		"Adult Social Care": 9300013.2,
-	// 		"Strategic Landlord": 5088515.55
-	// 	},
-	// 	{
-	// 		"City Development": 8784004.37,
-	// 		"Childrens Services": 11702386.14,
-	// 		"Central & Corporate": 7911010.72,
-	// 		"Joint Committees And Other Bodies": 3584194.1,
-	// 		"date": {
-	// 			"$date": 1420070400000
-	// 		},
-	// 		"Social Services Pooled Budgets": 6846886.97,
-	// 		"Environment & Housing": 10075489.42,
-	// 		"Strategic Accounts": 25948017.24,
-	// 		"Adult Social Care": 8473703.85,
-	// 		"Strategic Landlord": 8830333.22
-	// 	},
-	// 	{
-	// 		"Aire Valley Homes Leeds": 280.0,
-	// 		"City Development": 11952904.12,
-	// 		"Childrens Services": 10883582.69,
-	// 		"Central & Corporate": 8947542.47,
-	// 		"Joint Committees And Other Bodies": 3661200.1,
-	// 		"date": {
-	// 			"$date": 1404172800000
-	// 		},
-	// 		"Social Services Pooled Budgets": 8915671.07,
-	// 		"Environment & Housing": 15284473.45,
-	// 		"Strategic Accounts": 31803013.14,
-	// 		"Adult Social Care": 16492128.45,
-	// 		"East - North East": 50.0
-	// 	},
-	// 	{
-	// 		"City Development": 7132279.2,
-	// 		"Childrens Services": 12090592.0,
-	// 		"Central & Corporate": 7587193.0,
-	// 		"Joint Committees And Other Bodies": 3584194.1,
-	// 		"date": {
-	// 			"$date": 1414800000000
-	// 		},
-	// 		"Social Services Pooled Budgets": 6125454.18,
-	// 		"Environment & Housing": 12503327.88,
-	// 		"Strategic Accounts": 28685363.92,
-	// 		"Adult Social Care": 8976606.35,
-	// 		"Strategic Landlord": 4211179.04
-	// 	},
-	// 	{
-	// 		"City Development": 11147745.58,
-	// 		"Childrens Services": 16961142.09,
-	// 		"Central & Corporate": 8954887.02,
-	// 		"Joint Committees And Other Bodies": 3661200.1,
-	// 		"date": {
-	// 			"$date": 1412121600000
-	// 		},
-	// 		"Social Services Pooled Budgets": 7454900.89,
-	// 		"Environment & Housing": 15430787.83,
-	// 		"Strategic Accounts": 32231487.47,
-	// 		"Adult Social Care": 10459137.29,
-	// 		"Strategic Landlord": 121587.53
-	// 	},
-	// 	{
-	// 		"City Development": 8593659.27,
-	// 		"Childrens Services": 21886754.84,
-	// 		"Central & Corporate": 6844996.88,
-	// 		"Joint Committees And Other Bodies": 5261657.1,
-	// 		"date": {
-	// 			"$date": 1409529600000
-	// 		},
-	// 		"Social Services Pooled Budgets": 6221025.92,
-	// 		"Environment & Housing": 13997542.22,
-	// 		"Strategic Accounts": 33427171.59,
-	// 		"Adult Social Care": 8787961.8
-	// 	},
-	// 	{
-	// 		"Aire Valley Homes Leeds": 104.17,
-	// 		"City Development": 9641931.81,
-	// 		"West - North West": 27770.7,
-	// 		"Social Services Pooled Budgets": 5380175.28,
-	// 		"Childrens Services": 14320758.15,
-	// 		"Central & Corporate": 7723602.89,
-	// 		"Joint Committees And Other Bodies": 4880142.48,
-	// 		"date": {
-	// 			"$date": 1401580800000
-	// 		},
-	// 		"Other Education Services": 2147379.92,
-	// 		"Environment & Housing": 15457146.44,
-	// 		"Strategic Accounts": 31776186.17,
-	// 		"Adult Social Care": 8237285.09,
-	// 		"East - North East": 806.34
-	// 	},
-	// 	{
-	// 		"Aire Valley Homes Leeds": 54.0,
-	// 		"City Development": 7029421.59,
-	// 		"West - North West": 22566.65,
-	// 		"Social Services Pooled Budgets": 7035726.93,
-	// 		"Childrens Services": 8025143.81,
-	// 		"Central & Corporate": 8358859.0,
-	// 		"Joint Committees And Other Bodies": 3661200.1,
-	// 		"date": {
-	// 			"$date": 1398902400000
-	// 		},
-	// 		"Other Education Services": 783631.25,
-	// 		"Adults Social Care ": 75.0,
-	// 		"Environment & Housing": 10777050.5,
-	// 		"Strategic Accounts": 32072694.04,
-	// 		"Adult Social Care": 9494345.02,
-	// 		"East - North East": 671.91
-	// 	}
-	// ]
+		if (rep < 10000) { // place a comma between
+			return rep.charAt(0) + ',' + rep.substring(1);
+		}
+		if(rep < 1000000) { // display thousands with k
+			// divide and format
+			return (rep/1000).toFixed(rep % 1000 != 0)+'k';
+		}
+		return (rep/1000000).toFixed((rep % 1000000 != 0 ? 2 : 0))+'m';
+	}
 });
