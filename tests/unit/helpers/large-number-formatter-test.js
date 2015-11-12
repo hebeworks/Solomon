5f13d22004aa160ea762920223b528a5dbdf56dd
Nathan Smith <nathan@hebemedia.com>
2015-11-12 15:39:09 +0000

    Created a new large-number-formatter helper and removed the old number thousand format number helper to automatically display thousands with ks or millions with ms

---
+++ tests/unit/helpers/large-number-formatter-test.js
@@ -0,0 +1,10 @@
+import { largeNumberFormatter } from '../../../helpers/large-number-formatter';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | large number formatter');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = largeNumberFormatter(42);
+  assert.ok(result);
+});
