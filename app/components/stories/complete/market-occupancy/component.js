6e8a43aa84ac14312d09ba7db8dca07c9425e413
Nathan Smith <nathan@hebemedia.com>
2015-11-24 15:05:17 +0000

    Market Occupancy Story - loading data for average daily - bar chart

--- app/components/stories/complete/market-occupancy/component.js
+++ app/components/stories/complete/market-occupancy/component.js
@@ -6,7 +6,7 @@ export default DefaultStory.extend({
         title: 'Leeds Outside Markets', // (Provide a story title)
         subTitle: 'Statistics for Leeds city, Yeadon, Pudsey and Otley', // (Provide a story subtitle)
         author: 'Ste Allan',
-        
+
         description: 'A chart showing how market stall occupancy has changed over a 13-month period.', // (Provide a longer description of the story)
         license: 'UK Open Government Licence (OGL v2)', // (Define which license applies to usage of the story)
         dataSourceUrl: 'http://leedsdatamill.org/dataset/leeds-markets', // (Where did the data come from?)
@@ -16,16 +16,16 @@ export default DefaultStory.extend({
         slider: true, // (Add a horizontal slider to the story)
         scroll: false, // (Should the story vertically scroll its content?)
     },
-    
+
     loaded: false,
-    
+
     onInsertElement: function () {
         this.loadData();
     }.on('didInsertElement'),
-    
+
     loadData: function () {
         var _this = this;
-        var query = { $and: [{ "date" : { $lte : new Date("2014-03-31T00:00:00Z") } }, { "date" : { $gte : new Date("2014-03-01T00:00:00Z") } }]}; 
+        var query = { $and: [{ "date": { $lte: new Date("2014-03-31T00:00:00Z") } }, { "date": { $gte: new Date("2014-03-01T00:00:00Z") } }] };
         var url = this.get('hebeNodeAPI') + '/ldm-market-occupancy?query=' + this.convertQuery(query) + '&limit=-1';
         this.getData(url)
             .then(function (data) {
@@ -34,38 +34,85 @@ export default DefaultStory.extend({
                 }
             });
     },
-    
+
     parseData: function (data) {
         var _this = this;
         var chartData = [];
         chartData[0] = ['Day', 'Leeds', 'Pudsey', 'Otley', 'Yeadon'];
         // chartData[0] = ['Day', 'Leeds', 'Pudsey', 'Otley', 'Yeadon','tooltip'];
-        for(var i = 1; i <= 31; i ++) {
+        for (var i = 1; i <= 31; i++) {
             // [1,144,null,12,null];
-            chartData[i] = [i,null,null,null,null];
+            chartData[i] = [i, null, null, null, null];
             // chartData[i] = [i,null,null,null,null,moment(new Date("2014/03/"+i)).format('D MMM')];
         }
-        
+
         var occupancyData = JSON.parse(JSON.stringify(chartData));
         var percentageData = JSON.parse(JSON.stringify(chartData));
-        // var numberData = JSON.parse(JSON.stringify(chartData));
+                
+        // ================================================================================
+        // Calculate daily averages
+        // ================================================================================
+        function baseLocObj() {
+            return {
+                "Leeds": [],
+                "Pudsey": [],
+                "Otley": [],
+                "Yeadon": []
+            };
+        }
+        var days = { 
+            'Mon': baseLocObj(), 
+            'Tue': baseLocObj(), 
+            'Wed': baseLocObj(), 
+            'Thu': baseLocObj(), 
+            'Fri': baseLocObj(), 
+            'Sat': baseLocObj(), 
+            'Sun': baseLocObj() 
+        };
         
+        var dailyAverages = [chartData[0]];
         data.forEach(function (obj) {
             obj.occupied = obj.stalls_available - obj.stalls_void;
             obj.percent = obj.stalls_void / obj.stalls_available;
             var locationIndex = chartData[0].indexOf(obj.location);
             var dayOfMonth = moment(new Date(obj.date)).date();
-            occupancyData[dayOfMonth][locationIndex] = obj.occupied;            
-            percentageData[dayOfMonth][locationIndex] = obj.percent;            
+            occupancyData[dayOfMonth][locationIndex] = obj.occupied;
+            percentageData[dayOfMonth][locationIndex] = obj.percent;
+            
+            // Daily averages
+            var day = moment(new Date(obj.date)).format("ddd");
+            days[day][obj.location].push(obj.occupied);
         });
+        
+        function forEachProp(obj,callback) {
+            for(var prop in obj) {
+                callback(prop, obj[prop]);
+            }    
+        }
+        
+        // foreach day
+        forEachProp(days, function(day, dayObj){
+            var arr = [day];
+            // foreach location
+            forEachProp(dayObj,function(loc,locArr) {
+                // calculate the average            
+                var sum = _.reduce(locArr,function(mem,num){ return mem + num; });
+                var average = sum / locArr.length;
+                arr.push(average);
+            });
+            dailyAverages.push(arr);
+        });
+        // ================================================================================
+        
         this.setProperties({
-            occupancyData:occupancyData,
-            percentageData:percentageData
+            occupancyData: occupancyData,
+            percentageData: percentageData,
+            dailyAverages:dailyAverages
         });
         _this.set('loaded', true);
     },
-                
-    loadGoogleAPIs: function() {
+
+    loadGoogleAPIs: function () {
         // Draw the chart when the APIs have loaded
         google.setOnLoadCallback(
             this.drawTotalOccupiedStalls(),
@@ -73,8 +120,8 @@ export default DefaultStory.extend({
             this.drawAvgOccupiedStalls()
         );
     }.observes('loaded'),
-    
-    drawTotalOccupiedStalls: function() {
+
+    drawTotalOccupiedStalls: function () {
         var chartData = this.get('occupancyData');
         var data = new google.visualization.DataTable();
         data = google.visualization.arrayToDataTable(chartData);
@@ -130,8 +177,8 @@ export default DefaultStory.extend({
         var base64 = hebeutils.Base64.encode(json);
         return base64;
     },
-    
-    drawPCEmptyStalls: function() {
+
+    drawPCEmptyStalls: function () {
         var chartData = this.get('percentageData');
         var data = new google.visualization.DataTable();
         data = google.visualization.arrayToDataTable(chartData);
@@ -210,68 +257,40 @@ export default DefaultStory.extend({
 
         chart.draw(data, options);
     },
-    
-    drawAvgOccupiedStalls: function() {
-        var data = google.visualization.arrayToDataTable([
-            ['Location', 'Leeds City', 'Yeadon', 'Pudsey', 'Otley'],
-            ['Mon', 153, null, 12, 7],
-            ['Tue', 144, 8, 13, 10],
-            ['Wed', 43, 13, 17, null],
-            ['Thu', 43, 13, 17, 35],
-            ['Fri', 43, 13, 17, 22],
-            ['Sat', 43, 13, 17, null],
-            ['Sun', 43, 13, 17, 12],
-            ['Mon', 153, null, 12, 7],
-            ['Tue', 144, 8, 13, 10],
-            ['Wed', 43, 13, 17, null],
-            ['Thu', 43, 13, 17, 35],
-            ['Fri', 43, 13, 17, 22],
-            ['Sat', 43, 13, 17, null],
-            ['Sun', 43, 13, 17, 12],
-            ['Mon', 153, null, 12, 7],
-            ['Tue', 144, 8, 13, 10],
-            ['Wed', 43, 13, 17, null],
-            ['Thu', 43, 13, 17, 35],
-            ['Fri', 43, 13, 17, 22],
-            ['Sat', 43, 13, 17, null],
-            ['Sun', 43, 13, 17, 12],
-            ['Mon', 153, null, 12, 7],
-            ['Tue', 144, 8, 13, 10],
-            ['Wed', 43, 13, 17, null],
-            ['Thu', 43, 13, 17, 35],
-            ['Fri', 43, 13, 17, 22],
-            ['Sat', 43, 13, 17, null],
-            ['Sun', 43, 13, 17, 12]
-        ]);
+
+    drawAvgOccupiedStalls: function () {
+        var chartData = this.get('dailyAverages');
+        var data = new google.visualization.DataTable();
+        data = google.visualization.arrayToDataTable(chartData);
 
         var options = {
-          title: 'Daily Avg. Number of Occupied Stalls',
-          vAxis: {
-              baseline: 0,
-              minorGridlines: {
-                count: 4
-              }
-          },
-          chartArea: {
-              width: '90%',
-              height: '60%',
-              top: '20%',
-              left: '10%'
-          },
-          width: 290,
-          height: 390,
-          legend: {
-              position: 'top',
-              maxLines: '4'
-          },
-          isStacked: true,
-          hAxis: {
-              title: 'Day'
-          },
+            title: 'Daily Avg. Number of Occupied Stalls',
+            vAxis: {
+                baseline: 0,
+                minorGridlines: {
+                    count: 4
+                }
+            },
+            chartArea: {
+                width: '90%',
+                height: '60%',
+                top: '20%',
+                left: '10%'
+            },
+            width: 290,
+            height: 390,
+            legend: {
+                position: 'top',
+                maxLines: '4'
+            },
+            isStacked: true,
+            hAxis: {
+                title: 'Day of the week'
+            },
         };
 
         var chart = new google.visualization.ColumnChart(
-          document.getElementById('chart-avg-occupied-stalls'));
+            document.getElementById('chart-avg-occupied-stalls'));
 
         chart.draw(data, options);
     }
