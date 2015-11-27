1d7420f175c6d7707ab0b634cbc63454c5c4d71a
Nathan Smith <nathan@hebemedia.com>
2015-11-27 16:26:51 +0000

    Loading zones form API in canvas-settings

--- app/components/canvas-settings/component.js
+++ app/components/canvas-settings/component.js
@@ -1,10 +1,11 @@
-import Ember from 'ember';
+import dashComponentBase from 'hebe-dash/mixins/dash-component-base';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(dashComponentBase, {
 	appController:null,
 	canvasSettings: Ember.computed.alias('appSettings.canvasSettings'),
 
 	onDidReceiveAttrs: function () {
+		var _this = this;
 		this.set('appController.canvasSettings',
 			{
 				zones: [],
@@ -13,11 +14,16 @@ export default Ember.Component.extend({
 				startDate: moment(new Date()).subtract('month', 1).toDate(),
 				endDate: new Date(),
 			});
-		var zones = [];
-		for (var i = 1; i <= 50; i++) {
-			zones.push({ text: 'Zone ' + i, id: i });
-		}
-		this.set('canvasSettings.zones',zones);
+			var url = 'http://hebenodeapi-testing.azurewebsites.net/yw-zones?selectfields=name';
+			this.getData(url).then(function(data){
+				var zones = [];
+				if(!Ember.isEmpty(data)){
+					data.forEach(function(zone){
+						zones.push({ text: zone.name, id: zone._id });
+					});				
+				};
+				_this.set('canvasSettings.zones',zones);
+			});
 	}.on('didReceiveAttrs'),
 	
 });
