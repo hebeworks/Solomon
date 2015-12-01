f64f84df40b49bb7d830326c9df83eb33c6590bb
Ste Allan <ste@hebeworks.com>
2015-12-01 17:19:06 +0000

    Updated toolbar nav options.

--- app/canvas/controller.js
+++ app/canvas/controller.js
@@ -8,10 +8,10 @@ export default Ember.Controller.extend({
 
     navItems: function () {
         var items = [
-            { title: 'Toolbox', action: 'toggleToolbox' },
+            // { title: 'Toolbox', action: 'toggleToolbox' },
             // { title: 'Toolbox' },//, action: 'toggleSubNav' },
             { title: 'Filter', action: 'showCanvasSettings' },
-            { title: 'Gallery', action: 'gotoRoute', route: 'gallery' }
+            // { title: 'Gallery', action: 'gotoRoute', route: 'gallery' }
         ];
 
         for (var i = 0; i < items.length; i++) {
