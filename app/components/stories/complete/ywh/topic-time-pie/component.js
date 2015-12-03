9b01c87ec67e601491050552feb952da770a8147
Ste Allan <ste@hebeworks.com>
2015-12-03 11:48:55 +0000

    Converted topic pie chart to column chart.

--- app/components/stories/complete/ywh/topic-time-pie/component.js
+++ app/components/stories/complete/ywh/topic-time-pie/component.js
@@ -32,8 +32,9 @@ export default DefaultStory.extend({
     loadGoogleAPIs: function () {
         // Draw the chart when the APIs have loaded
         google.setOnLoadCallback(
-            this.drawDonutChart()
-            );
+            // this.drawDonutChart(),
+            this.drawChart()
+        );
     }.observes('loaded'),
 
     onTopicAttrs: function(){
@@ -69,41 +70,38 @@ export default DefaultStory.extend({
     }),
 
     ywData: Ember.computed.alias('appSettings.canvasSettings.ywData'),
-
-    drawDonutChart: function () {
-        // var data = google.visualization.arrayToDataTable([
-        //     ['Index', 'Rating'],
-        //     ['Index 1', 10],
-        //     ['Index 2', 18],
-        //     ['Index 3', 15],
-        //     ['Index 4', 5],
-        //     ['Index 5', 9]
-        // ]);
-
+    
+    drawChart: function() {
         var data = google.visualization.arrayToDataTable(this.get('chartData'));
-
+        
         var options = {
-            title: '',
-            pieHole: 0.3,
+            // title: 'Column Chart',
+            vAxis: {
+                // format: 'percent',
+                baseline: 0
+            },
             chartArea: {
-                width: '90%',
-                height: '90%',
-                top: '10%',
-                left: '10%'
+                width: '95%',
+                height: '80%',
+                top: '5%',
+                left: '0%'
             },
             width: 290,
-            height: 220,
+            height: 240,
             tooltip: {
                 isHtml: true
             },
-            // slices: {
-            //     2: {
-            //         offset: 0.1
-            //     }
-            // }
+            series: {
+                0: {
+                    color: 'rgb(70,142,229)'
+                }
+            }
         };
 
-        var chart = new google.visualization.PieChart(document.getElementById('google-donut-chart-single'));
+        var chart = new google.visualization.ColumnChart(document.getElementById('topic-time-chart'));
+        // var chart = new google.charts.Bar(document.getElementById('google-column-chart-single'));
+
         chart.draw(data, options);
+        // chart.draw(data, google.charts.Bar.convertOptions(options));
     }.observes('chartData')
 });
