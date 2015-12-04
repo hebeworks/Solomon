68ac8be6274f2a68b92830c44549ebdc089a8f75
Nathan Smith <nathan@hebemedia.com>
2015-12-04 11:43:54 +0000

    made closed on contact by max

--- app/components/stories/complete/ywh/topics-closed-duration-stacked-bar/component.js
+++ app/components/stories/complete/ywh/topics-closed-duration-stacked-bar/component.js
@@ -4,7 +4,7 @@ import DefaultStory from 'hebe-dash/components/stories/story-types/default-story
 export default DefaultStory.extend({
     storyConfig: {
         title: 'WQ Contacts Closed on Contact',
-        subTitle: 'Of the top needs, which are met in an hour',
+        subTitle: 'The needs with most calls closed on contact',
         scroll: false,
         viewOnly: true
     },
@@ -12,7 +12,8 @@ export default DefaultStory.extend({
     loadGoogleAPIs: function () {
         // Draw the chart when the APIs have loaded
         google.setOnLoadCallback(
-            this.drawStackedColumnChart()
+            // this.drawStackedColumnChart()
+            this.drawStackedColumnChartMax()
             );
     }.on('didInsertElement'),
 
@@ -22,77 +23,155 @@ export default DefaultStory.extend({
     }.on('didReceiveAttrs'),
 
 
-    drawStackedColumnChart: function () {
+    // drawStackedColumnChart: function () {
+    //     var data = this.get('ywData');
+
+    //     var chartData = [
+    //         ['Topic', '30m', '1hr', '+1hr']
+    //     ];
+
+    //     if (!Ember.isEmpty(data)) {
+    //         data.forEach(function (item) {
+    //             item.durationMins = moment.duration(item.duration).minutes();
+    //         });
+
+    //         var grouped = this.get('appSettings').groupSortCount(data,'need',5, false);
+    //         grouped.reverse();
+    //         var count = 0;
+            
+    //         for (var i = 0; i < grouped.length; i++) {
+    //             var prop = grouped[i].groupKey;
+                
+    //             if (count < 5) {
+    //                 var length = grouped[i].items.length;
+    //                 var items = grouped[i].items;
+                    
+    //                 var under30 = _.countBy(items, function (item) {
+    //                     return item.durationMins < 30;
+    //                 });
+    //                 under30 = under30.true;
+    //                 var under30P = parseInt((under30/length * 100).toString());
+                    
+    //                 var under60 = _.countBy(items, function (item) {
+    //                     return (item.durationMins >= 30 && item.durationMins < 60);
+    //                 });
+    //                 under60 = under60.true;
+    //                 var under60P = parseInt((under60P/length * 100).toString());
+                    
+    //                 var over60 = _.countBy(items, function (item) {
+    //                     return (item.durationMins >= 60);
+    //                 });
+    //                 over60 = over60.true;
+    //                 var over60P = parseInt((over60/length * 100).toString());
+                    
+    //                 chartData.push([prop, under30 || 0, under60 || 0, over60 || 0]);
+    //                 // chartData.push([prop, under30P || 0, under60P || 0, over60P || 0]);
+    //             }
+    //             count++;
+    //         }
+    //         var data = google.visualization.arrayToDataTable(chartData);
+    
+    //         var options = {
+    //             hAxis: {
+    //                 textPosition: 'none',
+    //                 direction: -1
+    //             },
+    //             vAxis: {
+    //                 baseline: 0
+    //             },
+    //             chartArea: {
+    //                 width: '80%',
+    //                 height: '85%',
+    //                 top: '10%',
+    //                 left: '10%'
+    //             },
+    //             width: 290,
+    //             height: 240,
+    //             legend: {
+    //                 position: 'top',
+    //                 maxLines: 4
+    //             },
+    //             isStacked: true,
+    //             tooltip: {
+    //                 isHtml: true
+    //             },
+    //             series: {
+    //                 0: {
+    //                     color: 'rgb(70,142,229)'
+    //                 },
+    //                 1: {
+    //                     color: 'rgb(89,172,0)'
+    //                 }
+    //             }
+    //         };
+
+    //         var chart = new google.visualization.ColumnChart(document.getElementById('google-stacked-column-chart-single1'));
+    //         // var chart = new google.charts.Bar(document.getElementById('google-stacked-column-chart-single'));
+
+    //         chart.draw(data, options);
+    //         // chart.draw(data, google.charts.Column.convertOptions(options));
+
+    //         this.set('loaded', true);
+    //     }
+    // }.observes('ywData'),
+    
+    
+    
+    
+    drawStackedColumnChartMax: function () {
         var data = this.get('ywData');
 
         var chartData = [
-            ['Topic', '30m', '1hr', '+1hr'] //, 'open'],
-            // ['Topic 1', 34, 5, 12, 7],
-            // ['Topic 2', 34, 5, 12, 7],
+            ['Topic', '30m', '1hr', '+1hr']
         ];
 
         if (!Ember.isEmpty(data)) {
             data.forEach(function (item) {
                 item.durationMins = moment.duration(item.duration).minutes();
             });
-            
-            // // group them by needs
-            // var grouped = _.groupBy(data, function (obj) {
-            //     return obj.need;
-            // });
 
-            // grouped = _.sortBy(grouped, function (obj) {
-            //     return obj.length;
-            // });
+            var grouped = this.get('appSettings').groupSortCount(data,'need');
 
-
-            var grouped = this.get('appSettings').groupSortCount(data,'need',5, false);
-            grouped.reverse();
-            var count = 0;
             
             for (var i = 0; i < grouped.length; i++) {
                 var prop = grouped[i].groupKey;
                 
-                if (count < 5) {
+
                     var length = grouped[i].items.length;
-                    
-                    
                     var items = grouped[i].items;
                     
-                    var under30 = _.countBy(items, function (item) {
-                        return item.durationMins < 30;
-                    });
+                    var under30 = _.countBy(items, function (item) { return item.durationMins < 30; });
                     under30 = under30.true;
-                    var under30P = parseInt((under30/length * 100).toString());
+                    // var under30P = parseInt((under30/length * 100).toString());
+                    grouped[i].under30 = under30;
                     
-                    var under60 = _.countBy(items, function (item) {
-                        return (item.durationMins >= 30 && item.durationMins < 60);
-                    });
+                    var under60 = _.countBy(items, function (item) { return (item.durationMins >= 30 && item.durationMins < 60); });
                     under60 = under60.true;
-                    var under60P = parseInt((under60P/length * 100).toString());
+                    grouped[i].under60 = under60;
+                    // var under60P = parseInt((under60P/length * 100).toString());
                     
-                    var over60 = _.countBy(items, function (item) {
-                        return (item.durationMins >= 60);
-                    });
+                    var over60 = _.countBy(items, function (item) { return (item.durationMins >= 60); });
                     over60 = over60.true;
-                    var over60P = parseInt((over60/length * 100).toString());
+                    grouped[i].over60 = over60;
+                    // var over60P = parseInt((over60/length * 100).toString());
                     
-                    chartData.push([prop, under30 || 0, under60 || 0, over60 || 0]);
+                    // chartData.push([prop, under30 || 0, under60 || 0, over60 || 0]);
                     // chartData.push([prop, under30P || 0, under60P || 0, over60P || 0]);
-                }
-                count++;
+
             }
-
-
-
-            var data = google.visualization.arrayToDataTable(chartData);
             
-            // var data = google.visualization.arrayToDataTable([
-            //     ['Location', 'Leeds City', 'Yeadon', 'Pudsey', 'Otley'],
-            //     ['2012', 34, 5, 12, 7],
-            //     ['2013', 41, 8, 13, 10],
-            //     ['2014', 43, 13, 17, 15]
-            // ]);
+            var sorted = _.sortBy(grouped,function(obj){
+               return obj.under30; 
+            });
+            sorted.reverse();
+            sorted.splice(5);
+            sorted.reverse();
+            for (var i = 0; i < sorted.length; i++) {
+                var item = sorted[i];                
+                chartData.push([item.groupKey, item.under30 || 0, item.under60 || 0, item.over60 || 0]);
+            }
+            
+            var data = google.visualization.arrayToDataTable(chartData);
     
             var options = {
                 hAxis: {
@@ -137,4 +216,5 @@ export default DefaultStory.extend({
             this.set('loaded', true);
         }
     }.observes('ywData')
+    
 });
