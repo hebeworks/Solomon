d7c8676ae44ad9ea6aa67fe8ecd47ae05c969c9b
Nathan Smith <nathan@hebemedia.com>
2015-12-10 12:59:12 +0000

    Added featured category search rather than a specific user to canvas lister and remove featured category from the category list tab

--- app/index/route.js
+++ app/index/route.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
     activate: function () {
         // var defaultCanvasID = 'leeds-city-council';
         var defaultCanvasID = 'zone-overview';
+        // var defaultCanvasID = 'contact-data';
         this.transitionTo('canvas', defaultCanvasID);
     }
 });
