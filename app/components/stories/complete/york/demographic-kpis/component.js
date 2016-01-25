ef3869d3e7f9f3d67397d604e9fcf09290d3b0d9
Ste Allan <ste@hebeworks.com>
2016-01-25 14:47:53 +0000

    Updates to demographic KPI story.

--- app/components/stories/complete/york/demographic-kpis/component.js
+++ app/components/stories/complete/york/demographic-kpis/component.js
@@ -1,10 +1,23 @@
 import BaseRAGTile from 'hebe-dash/components/stories/complete/base-rag-tile/component';
 
 export default BaseRAGTile.extend({
-    kpiReference: [], // KPI ID and description
-    kpiData: [], // actual
+    chosenKpi: 'BSC04', // will be altered by a select box when editing the story
     usableKpis: null,
-    chosenKpi: 'PHOF15', // set the ID of the selected KPI
+    shortenedKpis: [
+        {id: 'BSC03', description: '', short: false},
+        {id: 'BSC04', description: '', short: false},
+        {id: 'BYS001', description: '', short: false},
+        {id: 'BYS053', description: '', short: false},
+        {id: 'BYS079', description: '', short: false},
+        {id: 'BYS105', description: '', short: false},
+        {id: 'BYS131', description: '', short: false},
+        {id: 'BYS254', description: '', short: false},
+        {id: 'BYS300', description: '', short: false},
+        {id: 'BYS301', description: '', short: false},
+        {id: 'BYS302', description: '', short: false},
+        {id: 'PHOF15', description: '', short: false},
+        {id: 'PHOF26', description: '', short: false}
+    ], // manually curated KPI descriptions. Set short to 'true' if the description is short enough to display as larger text
     
     onInsertElement: function() {
         this.getData();
@@ -13,47 +26,21 @@ export default BaseRAGTile.extend({
     getData: function() {
         var _this = this;
         var data = {
-            resource_id: '04d56453-f3a5-4f19-94e0-ea1d9ddd6838', // the resource id
+            resource_id: '04d56453-f3a5-4f19-94e0-ea1d9ddd6838', // dataset ID
         };
+        
         $.ajax({
             url: 'https://data.yorkopendata.org/api/action/datastore_search',
             data: data,
             dataType: 'jsonp',
             success: function(data) {
-                // Create a list of KPI IDs and descriptions
-                var kpis = [],
-                    records = data.result.records;
+                var fullKpis = data.result.records; // Get out the actual full data
+                var strippedKpis = [];
                 
-                records.forEach(function(item) {
-                    kpis.push([
-                        item.Id,
-                        item.Indicator
-                    ]);
-                });
-                
-                kpis = _.groupBy(kpis, function(item) {
-                    return item[0];
-                });
-                
-                kpis = _.values(kpis);
-                
-                var filteredKpis = [];
-                kpis.forEach(function(item) {
-                    filteredKpis.push([
-                        item[0]
-                    ]);
-                });
-                
-                filteredKpis = _.flatten(filteredKpis, true);
-                
-                _this.set('kpiReference', filteredKpis);
-                
-                // Create a list of KPIs and their data
-                var kpiData = [];
-                
-                records.forEach(function(item) {
+                // Remove data objects we don't need
+                fullKpis.forEach(function(item) {
                     if (item.Total != '-' && item.Total != 'N/A') {
-                        kpiData.push({
+                        strippedKpis.push({
                             id: item.Id,
                             desc: item.Indicator,
                             period: item.Year,
@@ -63,21 +50,26 @@ export default BaseRAGTile.extend({
                     }
                 });
                 
-                var natesKpis = _.groupBy(kpiData, function(item) {
+                // Group KPI records by KPI ID
+                var groupedKpis = _.groupBy(strippedKpis, function(item) {
                     return item.id;
                 });
                 
-                var finalKPIs = {};
-                for(var id in natesKpis) {
-                    var items = natesKpis[id];
-
+                var finalKpis = {};
+                
+                // Use this grouped list and create singular KPI objects with exactly what we need
+                for(var id in groupedKpis) {
+                    var items = groupedKpis[id];
+                    
+                    // Use the second year of the period so we can find the previous period data
                     items = _.sortBy(items,function(obj){
                         return parseInt(obj.period.substr(obj.period.indexOf('/') + 1));
                     });
                     items.reverse();
+                    
                     var firstItem = items[0];
 
-                    finalKPIs[id] = {
+                    finalKpis[id] = {
                         desc: firstItem.desc,
                         polarity: firstItem.polarity,
                         currentVal: firstItem.value,
@@ -85,20 +77,18 @@ export default BaseRAGTile.extend({
                     }
                     
                     if(items.length > 1) {
-                        finalKPIs[id].previousVal = items[1].value;
-                        finalKPIs[id].previousPeriod = items[1].period;
+                        finalKpis[id].previousVal = items[1].value;
+                        finalKpis[id].previousPeriod = items[1].period;
                     }
                     
                     // This KPI has percentages listed as floats, so lets put '%' back on
                     if (id == 'PHOF15') {
-                        finalKPIs[id].currentVal = parseInt(firstItem.value) + '%';
-                        finalKPIs[id].previousVal = parseInt(items[1].value) + '%';
+                        finalKpis[id].currentVal = parseInt(firstItem.value) + '%';
+                        finalKpis[id].previousVal = parseInt(items[1].value) + '%';
                     }
                 }
                 
-                _this.set('usableKpis', finalKPIs);
-                
-                // console.log(finalKPIs);
+                _this.set('usableKpis', finalKpis);
                 
                 setTimeout(function () {
                     _this.set('loaded', true);
@@ -116,14 +106,22 @@ export default BaseRAGTile.extend({
                 _this.set('currentValue', kpis[id]['currentVal']);
                 _this.set('previousValue', kpis[id]['previousVal']);
                 _this.set('previousPeriod', kpis[id]['previousPeriod']);
-                _this.set('longText', kpis[id]['desc']);
+                
+                _this.shortenedKpis.forEach(function(item) {
+                    if (item.id == id) {
+                        if (item.short == true) {
+                            _this.set('shortText', item.description);
+                        } else {
+                            _this.set('longText', item.description);
+                        }
+                    }
+                });
                 
                 var polarity = kpis[id]['polarity'],
                     previousFloat = parseFloat(kpis[id]['previousVal']),
                     currentFloat = parseFloat(kpis[id]['currentVal']);
                     
                 if (currentFloat > previousFloat) {
-                    // console.log('Current is greater than previous');
                     _this.set('trend', 'up');
                     Ember.run.scheduleOnce('afterRender', this, grunticon.embedSVG);
                     
@@ -139,7 +137,6 @@ export default BaseRAGTile.extend({
                         _this.set('rating', 'neutral');
                     }
                 } else if (currentFloat < previousFloat) {
-                    // console.log('Current is less than previous');
                     _this.set('trend', 'down');
                     Ember.run.scheduleOnce('afterRender', this, grunticon.embedSVG);
                     
@@ -155,7 +152,6 @@ export default BaseRAGTile.extend({
                         _this.set('rating', 'neutral');
                     }
                 } else {
-                    // console.log('Current is the same as previous');
                     _this.set('rating', 'neutral');
                 }
             }
