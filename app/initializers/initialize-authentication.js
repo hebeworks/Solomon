d081a0a496895c9cfbbd57b18460e6ac8f04d1f4
Josh Nesbitt <josh@wearestac.com>
2016-02-02 15:25:26 +0000

    Simply header set.

--- app/initializers/initialize-authentication.js
+++ app/initializers/initialize-authentication.js
@@ -1,17 +1,7 @@
 // app/initializers/initialize-authentication.js
-export function initialize(container, application) {
 
+export function initialize(container, application){
   application.inject('adapter', 'session', 'simple-auth-session:main')
-
-  $.ajaxSetup({
-      beforeSend: function(xhr) {
-        var session = container.lookup('simple-auth-session:main');
-
-        if(session.get('isAuthenticated'))
-          xhr.setRequestHeader('Authorization', 'Bearer ' + session.get('secure.jwt'));
-      }
-  });
-
 }
 
 export default {
