28af5b38948dd0f8a5e49df1355b408ee5a62e71
Josh Nesbitt <josh@wearestac.com>
2016-02-03 17:19:12 +0000

    Show admin status and simplify attribute accessors in component.

--- app/components/session-manager/component.js
+++ app/components/session-manager/component.js
@@ -1,11 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-
-  profile: Ember.computed.alias('session.secure.profile'),
-
-  profileMeta: Ember.computed.alias('profile.user_metadata'),
-
-  profileAppMeta: Ember.computed.alias('profile.app_metadata')
-
-});
+export default Ember.Component.extend({});
