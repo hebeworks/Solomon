ed23a5909fab7f9102015c54a13a4b17ddc532b3
Josh Nesbitt <josh@wearestac.com>
2016-02-03 17:06:09 +0000

    Add attributes to user model.

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Object.extend({
 
+  id: Ember.computed.oneWay('user_id'),
+
+  email: null,
+
+  email_verified: null,
+
+  name: null,
+
+  picture: null,
+
+  created_at: null,
+
+  updated_at: null,
+
+  last_ip: null,
+
+  last_login: null,
+
+  logins_count: null,
+
+  username: function(){
+    var email = this.get('email');
+    var name = '';
+
+    if(email){
+      name = email.split('@')[0];
+    }
+
+    return name;
+  }.property('email'),
+
   isAdmin: function(){
     return false;
   }.property()
