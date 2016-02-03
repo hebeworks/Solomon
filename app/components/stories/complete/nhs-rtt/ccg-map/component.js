1c2d05dcf950cb149d25fb20ad60dd40539e132d
Nathan Smith <nathan@hebemedia.com>
2016-02-03 10:59:29 +0000

    Added basic click event to KML on Google Map

--- app/components/stories/complete/nhs-rtt/ccg-map/component.js
+++ app/components/stories/complete/nhs-rtt/ccg-map/component.js
@@ -15,17 +15,27 @@ export default DefaultStory.extend({
 
     onInsertElement: function () {
         var _this = this;
+        var kmlUrl = 'http://preview.mysolomon.co.uk/assets/CCG_BSC_Apr2015.kml';
         setTimeout(function () {
             var map = _this.get('gMap');
-            // debugger;
-            // map.setCenter({lat: 41.876, lng: -87.624});
-    //         zoom: 11,
-    // center: {lat: 41.876, lng: -87.624}
-            var ctaLayer = new google.maps.KmlLayer({
-                // url: 'http://googlemaps.github.io/js-v2-samples/ggeoxml/cta.kml',
-                url: 'http://preview.mysolomon.co.uk/assets/CCG_BSC_Apr2015.kml',
-                map: map.map
+
+            var kmlLayer = new google.maps.KmlLayer({
+                url: kmlUrl,
+                suppressInfoWindows: true,
+                map: map.map,
+                clickable: true
             });
+
+            kmlLayer.addListener('click', function (kmlEvent) {
+                var text = kmlEvent.featureData.name;
+                showInContentWindow(text);
+            });
+
+            function showInContentWindow(text) {
+                alert(text);
+                // var sidediv = document.getElementById('content-window');
+                // sidediv.innerHTML = text;
+            }
         }, 1000);
     }.on('didInsertElement')
 });
