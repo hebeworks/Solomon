6609b963edf74c7679aeee4b761681d478f40b5a
Josh Nesbitt <josh@wearestac.com>
2016-02-08 14:54:40 +0000

    Alias userID in list component.

--- app/components/canvas-gallery/lister-users/component.js
+++ app/components/canvas-gallery/lister-users/component.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 import CanvasGalleryLister from 'hebe-dash/mixins/canvas-gallery-lister';
 
-export default Ember.Component.extend(CanvasGalleryLister, {});
+export default Ember.Component.extend(CanvasGalleryLister, {
+
+  userID: Ember.computed.alias('currentUser.id')
+
+});
