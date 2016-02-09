9f7b5d0eab65889bee05c2cff9e4fd374104de07
Ste Allan <ste@hebeworks.com>
2016-02-09 14:56:32 +0000

    Progress with treatment RAG tile.

--- app/components/stories/complete/nhs-rtt/treatment-rag-tile/component.js
+++ app/components/stories/complete/nhs-rtt/treatment-rag-tile/component.js
@@ -5,45 +5,45 @@ export default DefaultStory.extend({
     // Story settings (including default values)
     // Uncomment any setting you need to change, delete any you don't need
     storyConfig: {
-        title: 'TITLE: nhs-rtt/treatment-rag-tile', // (Provide a story title)
-        subTitle: 'SUBTITLE: nhs-rtt/treatment-rag-tile', // (Provide a story subtitle)
-        // author: '', (Provide the author of the story)
-        
-        // description: '', // (Provide a longer description of the story)
-        // license: '', // (Define which license applies to usage of the story)
-        // dataSourceUrl: '', // (Where did the data come from?)
-        // feedbackEmail: '', // (Provide an email users can contact about this story)
-        
-        // color: 'white', // (Set the story colour)
-        // width: '2', // (Set the width of the story. If your story contains a slider, you must define the width, even if it is the same as the default.)
-        // height: '2', // (Set the height of the story)
-        // headerImage: '', // (Provide an image to show in the story header instead of the title and subtitle)
-        
-        // slider: false, // (Add a horizontal slider to the story)
-        // scroll: true, // (Should the story vertically scroll its content?)
+        title: '', // (Provide a story title)
+        subTitle: '', // (Provide a story subtitle)
+        width: '1', // (Set the width of the story. If your story contains a slider, you must define the width, even if it is the same as the default.)
+        height: '1', // (Set the height of the story)
+        scroll: false, // (Should the story vertically scroll its content?)
+        viewOnly: true
     },
     
-    // loaded: false, // (Tell other elements that this story has loaded)
-    //
-    
-    // Add your story-specific code here
-    data: null,
+    valueHasDeviated: true,
+    topHasChanged: false,
+    lowHasChanged: false,
+    topColour: 'black',
+    lowColour: 'black',
     
     onInsertElement: function () {
-        this.loadData();
-    }.on('didInsertElement'),
-
-    loadData: function () {
         var _this = this;
-        var url = 'http://'; // add any API url that returns JSON
-        this.getData()
-            .then(
-                function(data){
-                    var items = data; // the JSON returned from the API call is available here
-                    this.set('items',items); // set properties on the Ember component to make them available in the template
-                    setTimeout(() => { _this.set('loaded', true); });
-                },
-                function(err){ console.log(err); }
-            )
-    }
+        setTimeout(function() {
+            _this.set('loaded', true);
+        });
+    }.on('didInsertElement'),
+    
+    updateTileColours: function() {
+        var _this = this;
+        
+        if (_this.valueHasDeviated == true) {
+            console.log('valueHasDeviated');
+            _this.set('storyConfig.color', 'red');
+            _this.set('topColour', 'white');
+            _this.set('lowColour', 'white');
+        }
+        
+        if (_this.topHasChanged == true) {
+            console.log('topHasChanged');
+            _this.set('topColour', 'blue');
+        }
+        
+        if (_this.lowHasChanged == true) {
+            console.log('lowHasChanged');
+            _this.set('lowColour', 'red');
+        }
+    }.observes('loaded')
 });
