1a5af1f17e05af410755c9f829ca5319d7474dd5
Josh Nesbitt <josh@wearestac.com>
2016-02-11 15:13:41 +0000

    Make dirty tracking dynamic.

--- app/components/stories/edit-a-story/component.js
+++ app/components/stories/edit-a-story/component.js
@@ -5,24 +5,37 @@ export default Ember.Component.extend(BottomDrawerContent, {
 
 	message: '',
 
-	originalTitle: null,
+	editableFields: Ember.computed.alias('model.config'),
 
-	originalDescription: null,
+	editableFieldAttributes: function(){
+		return {};
+	}.property('editableFields'),
 
 	mainTitle: Ember.computed(function () {
 		return 'Edit a Story ' + this.get('model.title');
 	}),
 
-	onInit: function(){
+	storeEditableFieldValues: function(){
 		const fields = this.get('editableFields');
-		const title = fields.findBy('name', 'title');
-		const description = fields.findBy('name', 'description');
 
-		this.set('originalTitle', title.get('value'));
-		this.set('originalDescription', description.get('value'));
-	}.on('init'),
+		if(!fields)
+			return;
 
-	editableFields: Ember.computed.alias('model.config'),
+		fields.forEach(function(field){
+			this.set('editableFieldAttributes.' + field.get('name'), field.get('value'))
+		}.bind(this));
+	}.on('init').observes('editableFields'),
+
+	restoreEditableFieldValues: function(){
+		const fields = this.get('editableFields');
+
+		if(!fields)
+			return;
+
+		fields.forEach(function(field){
+			field.set('value', this.get('editableFieldAttributes.' + field.get('name')));
+		}.bind(this));
+	},
 
 	save: function () {
 		this.set('action', 'saveCanvasState');
@@ -37,13 +50,7 @@ export default Ember.Component.extend(BottomDrawerContent, {
 		},
 
 		cancel: function (){
-			const fields = this.get('editableFields');
-			const title = fields.findBy('name', 'title');
-			const description = fields.findBy('name', 'description');
-
-			title.set('value', this.get('originalTitle'));
-			description.set('value', this.get('originalDescription'));
-
+			this.restoreEditableFieldValues();
 			this.send('close');
 		},
 
