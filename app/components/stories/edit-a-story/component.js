d1ec870032a9460dce933aaac9e682b300e4eb6e
Josh Nesbitt <josh@wearestac.com>
2016-02-11 14:57:17 +0000

    Implement basic dirty model tracking when editing a story.

--- app/components/stories/edit-a-story/component.js
+++ app/components/stories/edit-a-story/component.js
@@ -5,12 +5,21 @@ export default Ember.Component.extend(BottomDrawerContent, {
 
 	message: '',
 
+	originalTitle: null,
+
+	originalDescription: null,
+
 	mainTitle: Ember.computed(function () {
 		return 'Edit a Story ' + this.get('model.title');
 	}),
 
 	onInit: function(){
-		this.get('model.config')
+		const fields = this.get('editableFields');
+		const title = fields.findBy('name', 'title');
+		const description = fields.findBy('name', 'description');
+
+		this.set('originalTitle', title.get('value'));
+		this.set('originalDescription', description.get('value'));
 	}.on('init'),
 
 	editableFields: Ember.computed.alias('model.config'),
@@ -28,6 +37,13 @@ export default Ember.Component.extend(BottomDrawerContent, {
 		},
 
 		cancel: function (){
+			const fields = this.get('editableFields');
+			const title = fields.findBy('name', 'title');
+			const description = fields.findBy('name', 'description');
+
+			title.set('value', this.get('originalTitle'));
+			description.set('value', this.get('originalDescription'));
+
 			this.send('close');
 		},
 
