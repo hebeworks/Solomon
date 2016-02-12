9f987530e127ffc5e0019ab59b5188052f3d23a7
Josh Nesbitt <josh@wearestac.com>
2016-02-12 09:42:04 +0000

    Create initial RTT news component.

---
+++ app/components/stories/complete/rtt-news/component.js
@@ -0,0 +1,12 @@
+/* global Ember, hebeutils, _ */
+import RSSGeneric from '../rss-generic/component';
+
+export default RSSGeneric.extend({
+
+    storyConfig: {
+        title: 'RTT News',
+        subTitle: 'Live RTT related news stories',
+        color: 'medium-blue'
+    }
+
+});
