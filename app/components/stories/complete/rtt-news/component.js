9e28d69b2556d364ccd2fcc91c43547a19f5a2e6
Ste Allan <ste@hebeworks.com>
2016-02-12 17:22:49 +0000

    Updated rtt news story.

--- app/components/stories/complete/rtt-news/component.js
+++ app/components/stories/complete/rtt-news/component.js
@@ -3,7 +3,7 @@ import RSSGeneric from '../rss-generic/component';
 
 export default RSSGeneric.extend({
 
-    defaultFeedURL: 'http://news.leeds.gov.uk/feed/en',
+    defaultFeedURL: 'http://www.nhs.uk/NHSChoices/shared/RSSFeedGenerator/RSSFeed.aspx?site=News',
 
     setSliderLoadedState: function(){
       Ember.run.next(this, function(){
@@ -12,13 +12,17 @@ export default RSSGeneric.extend({
     }.observes('loading'),
 
     storyConfig: {
-        title: 'In the news',
+        title: 'Behind the headlines',
         subTitle: '',
         color: 'white',
         scroll: false,
         slider: true,
         width: '2',
         height: '2'
-    }
+    },
+    
+    truncateText: function() {
+        this.$('[cpn-story_carousel-item] h3, [cpn-story_carousel-item] div *:first-child + *').dotdotdot({watch: true});
+    }.observes('loaded')
 
 });
