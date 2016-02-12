86c560ebf001e979daad4c0b2bb354506364f260
Josh Nesbitt <josh@wearestac.com>
2016-02-12 10:44:18 +0000

    Don't use default for now.

--- app/components/stories/complete/rtt-news/component.js
+++ app/components/stories/complete/rtt-news/component.js
@@ -3,7 +3,7 @@ import RSSGeneric from '../rss-generic/component';
 
 export default RSSGeneric.extend({
 
-    defaultFeedURL: 'https://www.google.com/alerts/feeds/14130490369006860511/11463334997055244251',
+    // defaultFeedURL: 'https://www.google.com/alerts/feeds/14130490369006860511/11463334997055244251',
 
     storyConfig: {
         title: 'RTT News',
