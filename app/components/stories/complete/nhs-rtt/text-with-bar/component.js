7af5c829fc98dbf19db7e42c17a1c06df9fe4ad8
Nathan Smith <nathan@hebemedia.com>
2016-02-13 13:51:55 +0000

    Ensure nhsrtt text-with-bar updates with the canvas filter changes

--- app/components/stories/complete/nhs-rtt/text-with-bar/component.js
+++ app/components/stories/complete/nhs-rtt/text-with-bar/component.js
@@ -29,27 +29,32 @@ export default DefaultStory.extend({
         });
     }.on('didInsertElement'),
 
+nhsFilter: Ember.computed.alias('appSettings.canvasSettings.nhsFilter'),
+
     loadData: function () {
         var _this = this;
-        var month = this.get('appSettings.canvasSettings.nhsFilter.selectedMonth');
-        var region = this.get('appSettings.canvasSettings.nhsFilter.selectedMonth');
+        var month = this.get('nhsFilter.selectedMonth');
+        var regionID = this.get('nhsFilter.selectedRegion.id');
 
         // var query = 'providerid=RQM&datekey=20150930';
-        var query = 'regionid=Y56&datekey=' + month.id + '&pathways=true';
-        var month = moment(month.id).format('MMM YYYY');
-        _this.set('month', month);
+        var query = 'regionid='+regionID+'&datekey=' + month.id + '&pathways=true';
+        var monthString = moment(month.id).format('MMM YYYY');
+        _this.set('month', monthString);
 
         this.getData(this.get('appSettings.hebeNodeAPI') + '/nhsrtt/waitinglist?' + query)
             .then(function(data) {
                     var items = data; // the JSON returned from the API call is available here
+                    var part2 = _.find(items,function(obj){
+                        return obj._id === 'PART_2';
+                    });
                     _this.set('items',items); // set properties on the Ember component to make them available in the template
-                    var percentage = ((items[3].gt_00_to_18_weeks_sum / items[3].total_all_sum) *100).toPrecisionDigits(1);
+                    var percentage = ((part2.gt_00_to_18_weeks_sum / part2.total_all_sum) *100).toPrecisionDigits(1);
                     _this.set('percentage', percentage);
                     setTimeout(() => { _this.set('loaded', true); });
                 },
                 function(err){ console.log(err); }
             )
-    },
+    }.observes('nhsFilter.selectedMonth','nhsFilter.selectedRegion'),
     
     // setValues: function() {
     //     this.set('percentage', 92.5);
