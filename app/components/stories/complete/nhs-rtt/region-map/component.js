5e7496073f0e03c364794c53c7705e03ee4a3ca7
Ste Allan <ste@hebeworks.com>
2016-02-13 20:46:05 +0000

    Clicking region map now selects the region.

--- app/components/stories/complete/nhs-rtt/region-map/component.js
+++ app/components/stories/complete/nhs-rtt/region-map/component.js
@@ -6,49 +6,63 @@ export default DefaultStory.extend({
     // Uncomment any setting you need to change, delete any you don't need
     storyConfig: {
         title: '', // (Provide a story title)
-        subTitle: '', // (Provide a story subtitle)
-        // author: '', (Provide the author of the story)
-        
-        // description: '', // (Provide a longer description of the story)
-        // license: '', // (Define which license applies to usage of the story)
-        // dataSourceUrl: '', // (Where did the data come from?)
-        // feedbackEmail: '', // (Provide an email users can contact about this story)
-        
+        subTitle: '', // (Provide a story subtitle)        
         color: 'blue', // (Set the story colour)
-        // width: '2', // (Set the width of the story. If your story contains a slider, you must define the width, even if it is the same as the default.)
-        // height: '2', // (Set the height of the story)
-        // headerImage: '', // (Provide an image to show in the story header instead of the title and subtitle)
-        
-        // slider: false, // (Add a horizontal slider to the story)
-        // scroll: true, // (Should the story vertically scroll its content?)
-        
-        // customProperties: '' // (Add custom values to the story attribute)
         viewOnly: true
     },
     
-    region: Ember.computed.alias('appSettings.canvasSettings.nhsFilter.selectedRegion._id'),
+    regions: Ember.computed.alias('appSettings.canvasSettings.nhsFilter.regions'),
+    selectedRegionObject: Ember.computed.alias('appSettings.canvasSettings.nhsFilter.selectedRegion'),
+    selectedRegionID: Ember.computed.alias('appSettings.canvasSettings.nhsFilter.selectedRegion._id'),
     
-    // loaded: false, // (Tell other elements that this story has loaded)
-    //
+    loaded: false, // (Tell other elements that this story has loaded)
     
-    // Add your story-specific code here
-    data: null,
-    
-    onInsertElement: function () {
-        this.loadData();
-    }.on('didInsertElement'),
-
-    loadData: function () {
+    onInsertElement: function () {        
         var _this = this;
-        var url = 'http://'; // add any API url that returns JSON
-        this.getData()
-            .then(
-                function(data){
-                    var items = data; // the JSON returned from the API call is available here
-                    this.set('items',items); // set properties on the Ember component to make them available in the template
-                    setTimeout(() => { _this.set('loaded', true); });
-                },
-                function(err){ console.log(err); }
-            )
-    }
+        setTimeout(function() {
+            _this.set('loaded', true);
+        });
+    }.on('didInsertElement'),
+    
+    updateRegion: function() {
+        var _this = this,
+            regions = this.get('regions'),
+            selectedRegionObject = this.get('selectedRegionObject');
+        
+        // London
+        _this.$('#Fill-9').on('click', function() {
+            var selectedRegion = _.find(regions, function(obj) {
+                return obj._id == 'Y56';
+            });
+            
+            _this.set('selectedRegionObject', selectedRegion);
+        });
+        
+        // South
+        _this.$('#Fill-4').on('click', function() {
+            var selectedRegion = _.find(regions, function(obj) {
+                return obj._id == 'Y57';
+            });
+            
+            _this.set('selectedRegionObject', selectedRegion);
+        });
+        
+        // North
+        _this.$('#Fill-1').on('click', function() {
+            var selectedRegion = _.find(regions, function(obj) {
+                return obj._id == 'Y54';
+            });
+            
+            _this.set('selectedRegionObject', selectedRegion);
+        });
+        
+        // Midlands
+        _this.$('#Fill-6').on('click', function() {
+            var selectedRegion = _.find(regions, function(obj) {
+                return obj._id == 'Y55';
+            });
+            
+            _this.set('selectedRegionObject', selectedRegion);
+        });
+    }.observes('loaded')
 });
