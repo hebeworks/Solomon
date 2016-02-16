046bc563d9e14d327e3d34ed8f852822b478ae5f
Nathan Smith <nathan@hebemedia.com>
2016-02-16 12:18:54 +0000

    Adding editable youtube_channel to youtube-generic

--- app/components/stories/complete/youtube-generic/component.js
+++ app/components/stories/complete/youtube-generic/component.js
@@ -14,44 +14,60 @@ export default DefaultStory.extend(EditableFields, {
         get() {
             return [
                 {
-                    name:"title",
-                    type:"text",
-                    value:'',
-                    placeholder:'Enter your title'
+                    name: "title",
+                    type: "text",
+                    value: '',
+                    placeholder: 'Enter your title'
                 },
                 {
                     name: 'description',
-                    type:'markdown',
-                    value:'',
-                    placeholder:'Enter your description'
+                    type: 'markdown',
+                    value: '',
+                    placeholder: 'Enter your description'
                 },
                 {
                     name: 'youtube_src',
-                    type:'text',
-                    value:'',
-                    placeholder:'Enter your youtube ID'
+                    type: 'text',
+                    value: '',
+                    placeholder: 'Enter your youtube ID'
+                },
+                {
+                    name: 'youtube_channel',
+                    type: 'text',
+                    value: '',
+                    placeholder: 'Enter your youtube channel'
                 }
             ];
         }
     }),
-    
-    title: Ember.computed('storyModel.config.@each.value',{
+
+    title: Ember.computed('storyModel.config.@each.value', {
         get() {
             return this.fetchEditableFieldValue('title');
         }
     }),
-    
-    description: Ember.computed('storyModel.config.@each.value',{
+
+    description: Ember.computed('storyModel.config.@each.value', {
         get() {
             return this.fetchEditableFieldValue('description');
         }
     }),
-    
-    youtubeSrc: Ember.computed('storyModel.config.@each.value',{
+
+    youtubeSrc: Ember.computed('storyModel.config.@each.value', {
         get() {
             var id = this.fetchEditableFieldValue('youtube_src');
-            if(!Ember.isEmpty(id)) {
+            if (!Ember.isEmpty(id)) {
                 return 'https://www.youtube.com/embed/' + id + '?rel=0&amp;showinfo=0';
+            }
+        }
+    }),
+
+    userChannel: Ember.computed('storyModel.config.@each.value', {
+        get() {
+            var id = this.fetchEditableFieldValue('youtube_channel');
+            if (!Ember.isEmpty(id)) {
+                // return 'https://www.youtube.com/embed/' + id + '?rel=0&amp;showinfo=0';
+                return 'http://www.youtube.com/embed/?listType=user_uploads&list=' + id;
             }
         }
     }),
