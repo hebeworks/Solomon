b865d9756bc1a190d35fe93b2812b38d37e3380f
Ste Allan <ste@hebeworks.com>
2016-02-19 15:08:51 +0000

    Added Air Table Text story skeleton.

--- app/components/stories/complete/air-table-text/component.js
+++ app/components/stories/complete/air-table-text/component.js
@@ -5,34 +5,25 @@ export default DefaultStory.extend({
     // Story settings (including default values)
     // Uncomment any setting you need to change, delete any you don't need
     initialConfig: {
-        title: 'TITLE: air-table-text', // (Provide a story title)
-        subTitle: 'SUBTITLE: air-table-text', // (Provide a story subtitle)
-        // author: '', (Provide the author of the story)
-        
-        // description: '', // (Provide a longer description of the story)
-        // license: '', // (Define which license applies to usage of the story)
-        // dataSourceUrl: '', // (Where did the data come from?)
-        // feedbackEmail: '', // (Provide an email users can contact about this story)
-        
-        // color: 'white', // (Set the story colour)
-        // width: '2', // (Set the width of the story. If your story contains a slider, you must define the width, even if it is the same as the default.)
-        // height: '2', // (Set the height of the story)
-        // headerImage: '', // (Provide an image to show in the story header instead of the title and subtitle)
-        
-        // slider: false, // (Add a horizontal slider to the story)
-        // scroll: true, // (Should the story vertically scroll its content?)
-        
-        // customProperties: '' // (Add custom values to the story attribute)
+        title: '', // (Provide a story title)
+        subTitle: '', // (Provide a story subtitle)
+        height: '1', // (Set the height of the story)
+        scroll: true, // (Should the story vertically scroll its content?)
+        viewOnly: true
     },
     
-    // loaded: false, // (Tell other elements that this story has loaded)
-    //
+    loaded: false, // (Tell other elements that this story has loaded)
+    content: null,
     
     // Add your story-specific code here
     data: null,
     
     onInsertElement: function () {
         this.loadData();
+        var _this = this;
+        setTimeout(function() {
+            _this.set('loaded', true);
+        });
     }.on('didInsertElement'),
 
     loadData: function () {
@@ -47,5 +38,12 @@ export default DefaultStory.extend({
                 },
                 function(err){ console.log(err); }
             )
-    }
+    },
+    
+    setContent: function() {
+        var content = 'Proin at nulla luctus leo rhoncus mollis. Ut lacinia massa ullamcorper nibh volutpat sollicitudin. Donec maximus dolor sit amet nulla rutrum, vel pellentesque magna aliquet. Integer pellentesque quam eget tempus blandit. Nam eget vestibulum.'
+        
+        this.set('storyConfig.title', 'Comments');
+        this.set('content', content);
+    }.observes('loaded')
 });
