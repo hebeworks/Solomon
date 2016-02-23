46e4434f15d9129b23ed53e44fc29c1b111b1f7c
Ste Allan <ste@hebeworks.com>
2016-02-23 17:01:08 +0000

    Added skeleton quick search story.

--- app/components/stories/complete/bid/quick-search/component.js
+++ app/components/stories/complete/bid/quick-search/component.js
@@ -5,24 +5,11 @@ export default DefaultStory.extend({
     // Story settings (including default values)
     // Uncomment any setting you need to change, delete any you don't need
     initialConfig: {
-        title: 'TITLE: bid/quick-search', // (Provide a story title)
-        subTitle: 'SUBTITLE: bid/quick-search', // (Provide a story subtitle)
-        // author: '', (Provide the author of the story)
-        
-        // description: '', // (Provide a longer description of the story)
-        // license: '', // (Define which license applies to usage of the story)
-        // dataSourceUrl: '', // (Where did the data come from?)
-        // feedbackEmail: '', // (Provide an email users can contact about this story)
-        
-        // color: 'white', // (Set the story colour)
-        // width: '2', // (Set the width of the story. If your story contains a slider, you must define the width, even if it is the same as the default.)
-        // height: '2', // (Set the height of the story)
-        // headerImage: '', // (Provide an image to show in the story header instead of the title and subtitle)
-        
-        // slider: false, // (Add a horizontal slider to the story)
-        // scroll: true, // (Should the story vertically scroll its content?)
-        
-        // customProperties: '' // (Add custom values to the story attribute)
+        title: 'Quick Search', // (Provide a story title)
+        subTitle: 'Some description about quick search', // (Provide a story subtitle)
+        height: '1', // (Set the height of the story)
+        viewOnly: true,
+        showHeaderBorder: false
     },
     
     // loaded: false, // (Tell other elements that this story has loaded)
