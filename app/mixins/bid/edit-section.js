e26af7828a993ba03f25fbf4da2bd299a7de6330
Ste Allan <ste@hebeworks.com>
2016-03-10 17:16:22 +0000

    Styled edit sections and added input fields.

--- app/mixins/bid/edit-section.js
+++ app/mixins/bid/edit-section.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     sectionOpenState: 'is-closed',
     
+    embedSectionGraphic: function() {
+        console.log('embedSectionGraphic');
+        Ember.run.scheduleOnce('afterRender', this, grunticon.embedSVG);
+    }.on('init'),
+    
     actions: {
         toggleSection: function() {
             if (this.get('sectionOpenState') == 'is-closed') {
