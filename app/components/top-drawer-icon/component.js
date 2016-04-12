b514e31ab4d0aad535c97b4d2da6a9dfbf99aa19
Nathan Smith <nathan@hebemedia.com>
2016-04-12 10:53:07 +0100

    passing route property to send action in top drawer icon component

--- app/components/top-drawer-icon/component.js
+++ app/components/top-drawer-icon/component.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-		click: function() {
-			this.sendAction('action');
-		}
+  click() {
+    this.sendAction('action', this.get('route'));
+  },
 });
