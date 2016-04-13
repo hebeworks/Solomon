c16308805dfb5ae0db900a4cb7c64daee07c2222
Ste Allan <ste@hebeworks.com>
2016-04-13 15:17:53 +0100

    Added basic gauge to Bin Levels story.

--- app/components/stories/complete/binnovation/bin-levels/component.js
+++ app/components/stories/complete/binnovation/bin-levels/component.js
@@ -9,30 +9,40 @@ export default DefaultStory.extend({
         subTitle: '', // (Provide a story subtitle)
         scroll: false, // (Should the story vertically scroll its content?)
         showHeaderBorder: false,
-        viewOnly: true
+        viewOnly: true,
+        showLoading: true
     },
     
-    // loaded: false, // (Tell other elements that this story has loaded)
-    //
+    loaded: true,
     
-    // Add your story-specific code here
-    data: null,
-    
-    onInsertElement: function () {
-        this.loadData();
-    }.on('didInsertElement'),
-
-    loadData: function () {
-        var _this = this;
-        var url = 'http://'; // add any API url that returns JSON
-        this.getData()
-            .then(
-                function(data){
-                    var items = data; // the JSON returned from the API call is available here
-                    this.set('items',items); // set properties on the Ember component to make them available in the template
-                    setTimeout(() => { _this.set('loaded', true); });
-                },
-                function(err){ console.log(err); }
-            )
-    }
+    fillPercentage: 35,
+    fillPercentageRemaining: Ember.computed('fillPercentage', function fillPercentageRemaining() {
+        const fillPercentage = this.get('fillPercentage');
+        
+        return 100 - fillPercentage;
+    }),
+    fillText: Ember.computed('fillPercentage', function fillText() {
+        const fillPercentage = this.get('fillPercentage');
+        
+        if (fillPercentage >= 80) {
+            return 'Getting full<br>Keep an eye on me';
+        } else {
+            return 'Plenty of room<br>Keep it coming';
+        }
+    }),
+    fillColour: Ember.computed('fillPercentage', function fillColour() {
+        const fillPercentage = this.get('fillPercentage');
+        
+        if (fillPercentage >= 80) {
+            return 'orange';
+        } else {
+            return 'green';
+        }
+    }),
+    recyclingTrend: null, // more, same, less
+    // batteryStatus: null, // can't write this until we get the sensor
+    // connectionStatus: null, // can't write this until we get the sensor
+    previousBinDay: null,
+    nextBinDay: null,
+    currentDate: null,
 });
