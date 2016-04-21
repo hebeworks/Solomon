4a627239fdf9fac126b35465052f2b0f022dd265
Nathan Smith <nathan@hebemedia.com>
2016-04-21 16:38:17 +0100

    code formatting on current-user initializer

--- app/instance-initializers/current-user.js
+++ app/instance-initializers/current-user.js
@@ -1,31 +1,27 @@
+/* globals Ember */
 // app/instance-initializers/current-user.js
-
 import User from 'hebe-dash/models/user';
 
 export default {
-
-  name: "current-user",
-
-  initialize: function({ registry }) {
+  name: 'current-user',
+  initialize({ registry }) {
     const session = registry.lookup('simple-auth-session:main');
-    var user = User.create(session.get('secure.profile'));
+    let user = User.create(session.get('secure.profile'));
     const proxy = Ember.ObjectProxy.create({
       isServiceFactory: true,
-      content: user
+      content: user,
     });
 
-    function syncObjects(){
+    function syncObjects() {
       const profile = session.get('secure.profile');
-
-      if(!profile){
+      if (!profile) {
         user.destroy();
         user = User.create();
         proxy.set('content', user);
-      }
-      else {
+      } else {
         user.setProperties(profile);
       }
-    };
+    }
 
     Ember.addObserver(session, 'secure', this, syncObjects);
 
@@ -33,6 +29,5 @@ export default {
     registry.injection('route', 'currentUser', 'service:current-user');
     registry.injection('controller', 'currentUser', 'service:current-user');
     registry.injection('component', 'currentUser', 'service:current-user');
-  }
-
+  },
 };
