ee243565ec443ef5849eb085c3b3112fbfc93048
Nathan Smith <nathan@hebemedia.com>
2016-04-25 15:36:16 +0100

    added the new getData methods to dash-component mixin

--- app/mixins/dash-component-base.js
+++ app/mixins/dash-component-base.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 import config from 'hebe-dash/config/environment';
 
 export default Ember.Mixin.create({
-	Config: config.APP,
-	getData: function (url, cache) {
-		return this.get('appSettings').getData(url, cache);
-	}
+  Config: config.APP,
+  postData: function postData(url, data, authenticated) {
+    return this.get('appSettings').getData(url, false, 'POST', data, authenticated);
+  },
+  getData: function getData(url, cache, authenticated, data) {
+    return this.get('appSettings').getData(url, cache, 'GET', data, authenticated);
+  },
 });
