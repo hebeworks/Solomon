17becad1b40af81bf825e72a266cad14cb41c19d
Nathan Smith <nathan@hebemedia.com>
2016-04-28 18:20:31 +0100

    cleaned up current user initializer

--- app/instance-initializers/current-user.js
+++ app/instance-initializers/current-user.js
@@ -1,46 +1,12 @@
 /* globals Ember */
 // app/instance-initializers/current-user.js
 import User from 'hebe-dash/models/user';
-import config from 'hebe-dash/config/environment';
 
 export default {
   name: 'current-user',
   initialize({ registry }) {
     const session = registry.lookup('simple-auth-session:main');
-    const appSettings = registry.lookup('service:solomon-settings');
-    const solomonAPIURL = config.APP.solomonAPIURL;
     let user = User.create(session.get('secure.profile'));
-
-    // function loadUser(baseUser) {
-    //   if (baseUser) {
-    //     const userID = baseUser.user_id;
-    //     if (userID) {
-    //       // load the user from the solomon api
-    //       const headers = [{ key: 'Solomon-Client-Override',
-    //                         value: config.APP.solomonClientOverride }];
-    //       appSettings.getData(`${solomonAPIURL}/api/users/${userID}`,
-    //                             false, 'get', null, true, headers)
-    //       .then(
-    //         (foundUser) => {
-    //           // add the metadata to the user
-    //           // TODO make this persistent - store in session
-    //           //    currently lost on reload
-    //           user.set('metaData', foundUser.metaData);
-    //         },
-    //         (/* err */) => {
-    //           user.setProperties(baseUser);
-    //           // appSettings.set('errorMessage',
-    //           //   'Sorry, your user was not loaded correctly, please try log out and back in.')
-    //         }
-    //       );
-    //     }
-    //   }
-    // }
-
-    // if (user) {
-    //   loadUser(user);
-    // }
-
     const proxy = Ember.ObjectProxy.create({
       isServiceFactory: true,
       content: user,
@@ -54,7 +20,6 @@ export default {
         proxy.set('content', user);
       } else {
         user.setProperties(profile);
-        // loadUser(profile);
       }
     }
 
