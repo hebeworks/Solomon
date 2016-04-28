435753bf607db823043c5567a0132039161f2c72
Nathan Smith <nathan@hebemedia.com>
2016-04-28 12:45:24 +0100

    current user initializer will now attempt to load the user model from the new endpoint in Solomon API

--- app/instance-initializers/current-user.js
+++ app/instance-initializers/current-user.js
@@ -1,11 +1,16 @@
 /* globals Ember */
 // app/instance-initializers/current-user.js
 import User from 'hebe-dash/models/user';
+import config from 'hebe-dash/config/environment';
 
 export default {
   name: 'current-user',
   initialize({ registry }) {
+
     const session = registry.lookup('simple-auth-session:main');
+    const appSettings = registry.lookup('service:solomon-settings');
+    const solomonAPIURL = config.APP.solomonAPIURL;
+
     let user = User.create(session.get('secure.profile'));
     const proxy = Ember.ObjectProxy.create({
       isServiceFactory: true,
@@ -19,7 +24,24 @@ export default {
         user = User.create();
         proxy.set('content', user);
       } else {
-        user.setProperties(profile);
+        const userID = profile.user_id;
+        if (userID) {
+          // load the user from the solomon api
+          const headers = [{ key: 'Solomon-Client-Override',
+                              value: config.APP.solomonClientOverride }];
+          appSettings.getData(`${solomonAPIURL}/api/users/${userID}`,
+                                  false, 'get', null, true, headers)
+          .then(
+            (foundUser) => {
+              user.setProperties(foundUser);
+            },
+            (/* err */) => {
+              user.setProperties(profile);
+              // appSettings.set('errorMessage',
+              //   'Sorry, your user was not loaded correctly, please try log out and back in.')
+            }
+          );
+        }
       }
     }
 
