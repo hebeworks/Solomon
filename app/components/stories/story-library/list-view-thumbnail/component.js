b37e42a952de0fb9d141d6751765bd723793ad29
Nathan Smith <nathan@hebemedia.com>
2016-04-29 19:06:33 +0100

    formatted list-view-thumbnail and changes to user solomon service isAllowed to check delete permissions using a promise rather than computed property

--- app/components/stories/story-library/list-view-thumbnail/component.js
+++ app/components/stories/story-library/list-view-thumbnail/component.js
@@ -1,37 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'li',
-    attributeBindings: ['cpn-thumb-list_item'],
-    'cpn-thumb-list_item': '',
-	
-	click: function() {
-		this.sendAction('action', this.model);
-	},
-    
-    canDelete: Ember.computed('model', function() {
-        const modelType = this.model.get('constructor').typeKey;
-        
-        const currentUser = this.get('currentUser');
-        const allowedScope = this.model.get('id');
-        const allowedModelType = 'canvas';
-        const allowedAction = 'delete';
-        
-        if (modelType === 'canvas') {
-            if (currentUser.get('isAllowed')(allowedScope, allowedModelType, allowedAction)) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }),
-    
-    actions: {
-        deleteCanvas(model) {
-            // this.set('action', 'deleteCanvas');
-            // this.send('action', model);
-        }
+  tagName: 'li',
+  attributeBindings: ['cpn-thumb-list_item'],
+  'cpn-thumb-list_item': '',
+
+  canDelete: false,
+  fetchIfCanDelete: function fetchIfCanDelete() {
+    const _this = this;
+    if (this.get('model')) {
+      const modelType = this.model.get('constructor').typeKey;
+      const allowedScope = this.model.get('id');
+      const allowedModelType = 'canvas';
+      const allowedAction = 'delete';
+
+      if (modelType === 'canvas') {
+        this.get('appSettings')
+          .isAllowed(allowedScope, allowedModelType, allowedAction)
+            .then(
+              (isAllowed) => {
+                _this.set('canDelete', isAllowed);
+              },
+              (/* err */) => {
+                _this.set('canDelete', false);
+              }
+            );
+      }
     }
+  }.on('didReceiveAttrs').observes('model'),
+
+  click() {
+    this.sendAction('action', this.model);
+  },
+
+  actions: {
+    deleteCanvas(model) {
+        // this.set('action', 'deleteCanvas');
+        // this.send('action', model);
+    }
+  }
 });
