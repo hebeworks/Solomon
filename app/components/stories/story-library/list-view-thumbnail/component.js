8bdbf3a041f3ba502a5e1272832da9219705992d
Ste Allan <ste@hebeworks.com>
2016-04-29 13:41:04 +0100

    Updated delete canvas functionality:
    Thumbnail component now checks if user can delete a canvas before rendering the delete button;

--- app/components/stories/story-library/list-view-thumbnail/component.js
+++ app/components/stories/story-library/list-view-thumbnail/component.js
@@ -9,6 +9,25 @@ export default Ember.Component.extend({
 		this.sendAction('action', this.model);
 	},
     
+    canDelete: Ember.computed('model', function() {
+        const modelType = this.model.get('constructor').typeKey;
+        
+        const currentUser = this.get('currentUser');
+        const allowedScope = this.model.get('id');
+        const allowedModelType = 'canvas';
+        const allowedAction = 'delete';
+        
+        if (modelType === 'canvas') {
+            if (currentUser.get('isAllowed')(allowedScope, allowedModelType, allowedAction)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }),
+    
     actions: {
         deleteCanvas(model) {
             // this.set('action', 'deleteCanvas');
