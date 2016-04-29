bfc9d70b3897ce33b8153e46d336d33d39b6fb93
Nathan Smith <nathan@hebemedia.com>
2016-04-29 10:15:30 +0100

    environment config setup

--- config/environment.js
+++ config/environment.js
@@ -98,7 +98,7 @@ module.exports = function config(environment) {
     ENV.APP.mockSolomonHostname = 'bid.preview.mysolomon.co.uk'; //'leeds.preview.mysolomon.co.uk';
     ENV.APP.solomonClientOverride = 'solomon_bid_leeds'; // solomon_production solomon_leeds
     ENV['auth0-ember-simple-auth'] = auth0Configs.bid;
-    ENV.APP.solomonAPIURL = 'http://testing.api.mysolomon.co.uk';
+    // ENV.APP.solomonAPIURL = 'http://localhost:3000';
   }
 
   if (environment === 'test') {
@@ -114,7 +114,8 @@ module.exports = function config(environment) {
   }
 
   if (environment === 'production') {
-    ENV['auth0-ember-simple-auth'] = auth0Configs.production;
+    ENV['auth0-ember-simple-auth'] = auth0Configs.bid;
+    ENV.APP.solomonAPIURL = 'http://testing.api.mysolomon.co.uk';
   }
 
   ENV.googleMap = {
